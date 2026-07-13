2400d8297d4cd8ae25cf7386bafcd7fe12e404a8
Serghini omar <106280687+omar1111eamsa@users.noreply.github.com>
2026-07-13 03:35:28 +0100

    Add kubectl provider configuration

--- dev/providers.tf
+++ dev/providers.tf
@@ -22,6 +22,10 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.12"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
   }
 }
 
@@ -45,4 +49,11 @@ provider "helm" {
     client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
   }
+}
+provider "kubectl" {
+  host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
+  load_config_file       = false
 }
