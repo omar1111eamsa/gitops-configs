78e7a078d2890a506dfc9c9a5192cbff5e057b29
Serghini omar <106280687+omar1111eamsa@users.noreply.github.com>
2026-07-13 03:41:13 +0100

    Add ArgoCD Kubernetes namespace and Helm release

--- dev/argocd.tf
+++ dev/argocd.tf
@@ -8,3 +8,39 @@ resource "kubernetes_namespace" "app" {
 
   depends_on = [time_sleep.wait_for_cluster]
 }
+
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+    labels = {
+      "app.kubernetes.io/name" = "argocd"
+    }
+  }
+
+  depends_on = [time_sleep.wait_for_cluster]
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  version    = "6.7.3"
+
+  timeout       = 600
+  wait          = true
+  wait_for_jobs = true
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "configs.params.server\\.insecure"
+    value = "true"
+  }
+
+  depends_on = [kubernetes_namespace.argocd]
+}
