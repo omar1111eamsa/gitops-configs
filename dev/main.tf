9f5ea84101da8695def194fa4a25962da6cd7c54
Serghini omar <106280687+omar1111eamsa@users.noreply.github.com>
2026-07-13 03:08:28 +0100

    Fix formatting in main.tf resource group name

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "main" {
-  name = "${var.resource_group_name}-{var.environment}"
+  name     = "${var.resource_group_name}-${var.environment}"
   location = var.location
-  tags = var.tags
+  tags     = var.tags
 }
