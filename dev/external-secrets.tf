71415c0394d21e7a517447afe7154905606307e7
Serghini omar <106280687+omar1111eamsa@users.noreply.github.com>
2026-07-13 03:36:33 +0100

    Add SecretStore resource for Azure Key Vault

--- dev/external-secrets.tf
+++ dev/external-secrets.tf
@@ -24,3 +24,26 @@ resource "helm_release" "external_secrets" {
 
   depends_on = [kubernetes_namespace.external_secrets]
 }
+
+
+resource "kubectl_manifest" "secret_store" {
+  yaml_body = <<-YAML
+    apiVersion: external-secrets.io/v1beta1
+    kind: SecretStore
+    metadata:
+      name: azure-keyvault-store
+      namespace: 3tirewebapp-dev
+    spec:
+      provider:
+        azurekv:
+          authType: ManagedIdentity
+          identityId: ${azurerm_kubernetes_cluster.main.kubelet_identity[0].client_id}
+          vaultUrl: ${azurerm_key_vault.main.vault_uri}
+          tenantId: ${data.azurerm_client_config.current.tenant_id}
+  YAML
+
+  depends_on = [
+    helm_release.external_secrets,
+    azurerm_key_vault_access_policy.aks_kubelet
+  ]
+}
